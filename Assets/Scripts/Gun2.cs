025e0b1f79d5945b2e6bcf48069316963972af7c
Nick Hatcher <nick@colossalrobot.com>
2020-01-29 16:56:23 -0500

    Update Gun2.cs

--- Assets/Scripts/Gun2.cs
+++ Assets/Scripts/Gun2.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class Gun2 : Gun
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    public Camera fpsCam;
+    private float nextTimeToFire;
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            Debug.Log("Bullet, Bullet, Bullet!");
+            Shoot();
+        }
+    }
+    void Shoot()
+    {
+        muzzleFlash.Play();
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, gunRange))
+        {
+            Debug.Log("shoots: " + hit.transform.name);
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(gunDamage);
+            }
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+            GameObject impactGO = Instantiate(impactFX, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 2f);
+
+        }
     }
 }
