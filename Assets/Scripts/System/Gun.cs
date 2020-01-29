27d59b35a6a5f8eb9e7f1cef1ff2218a054cc509
Nick Hatcher Alpha <nick@colossalrobot.com>
2020-01-29 07:38:08 -0500

    Milk and Bacon Update

---
+++ Assets/Scripts/System/Gun.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float gunDamage;
+    public float gunRange;
+
+    public Camera fpsCam;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire 1"))
+        {
+            Shoot();
+        }
+    }
+    void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, gunRange))
+        {
+            Debug.Log(this.name + " shoots " + hit.transform.name);
+        }
+    }
+}
