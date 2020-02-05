217cc577650dd91048f3448cfd5367d271603772
Nick Hatcher <nick@colossalrobot.com>
2020-02-05 13:19:09 -0500

    Update Target.cs

--- Assets/Scripts/System/Target.cs
+++ Assets/Scripts/System/Target.cs
@@ -3,6 +3,13 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     public float health = 50f;
+    public float deathHeight=50f;
+    void Update(){
+        if(transform.position.y <= -deathHeight)
+        {
+        Die();
+        }
+    }
     public void TakeDamage(float amount)
     {
         health -= amount;
