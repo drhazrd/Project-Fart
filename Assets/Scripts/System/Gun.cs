9e9ce76969bbf7a61ad1aa1ac1b972e5c623a51a
Nick Hatcher <nick@colossalrobot.com>
2020-02-05 13:23:50 -0500

    Update Gun.cs

--- Assets/Scripts/System/Gun.cs
+++ Assets/Scripts/System/Gun.cs
@@ -10,7 +10,5 @@ public class Gun : MonoBehaviour
     public float fireRate;
     public GameObject muzzleFlash;
     public GameObject impactFX;
-    public int maxAmmo;
-    public int currAmmo;
     public float reloadTime = 1f;
 }
