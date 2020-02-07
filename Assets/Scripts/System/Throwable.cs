5bb99015d9528ecf4f9109555489939ec3d5648c
Nick Hatcher <nick@colossalrobot.com>
2020-02-07 17:01:21 -0500

    Update Throwable.cs

--- Assets/Scripts/System/Throwable.cs
+++ Assets/Scripts/System/Throwable.cs
@@ -1,18 +1,46 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Throwable : MonoBehaviour
 {
+    public float delay = 3f;
+    public float radius = 3f;
+    public float explosionForce = 700f;
+    float countDown;
+    bool hasExploded = false;
+    public GameObject explosionFX;
     // Start is called before the first frame update
     void Start()
     {
-        
+        countdown = delay;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        countDown-=Time.deltaTime;
+        if(countdown <= 0f && hasExploded){
+            Expolde();
+            hasExploded = true;
+        }
+    }
+    void Expolde(){
+        Debug.Log("BOOOM!!");
+        Instantiate(explosionFX, transform.position, tranform.rotation);
+        Collider[] collidersToDestroy = Physics.OverlapSphere(transform.position, radius);
+        foreach(Collider nearbyObj in collidersToDestroy)
+        {
+            
+        }
+        Collider[] collidersToMove = Physics.OverlapSphere(transform.position, radius);
+        foreach(Collider nearbyObj in collidersToMove)
+        {
+            Rigidbody rb = nearbyObj.GetComponent<Rigidbody>();
+            if(rb != null){
+                rb.AddExplosionForce(explosionForce, transfor.position, radius);
+            }
+        }
+        Destroy(gameObject);
     }
 }
