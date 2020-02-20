d66ab63159e1b12e04d1c743b4004b7f229aa012
Nick Hatcher <nick@colossalrobot.com>
2020-02-20 11:40:37 -0500

    Update PlayerMotor.cs

--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -60,6 +60,16 @@ public class PlayerMotor : MonoBehaviour
         GetComponent<Rigidbody>().AddForce(new Vector3(0, -gravity * GetComponent<Rigidbody>().mass, 0));
 
         grounded = false;
+        
+        if (Input.GetButtonDown("Jump") && _isGrounded)
+        {
+            _body.AddForce(Vector3.up * Mathf.Sqrt(JumpHeight * -2f * Physics.gravity.y), ForceMode.VelocityChange);
+        }
+        if (Input.GetButtonDown("Dash"))
+        {
+            Vector3 dashVelocity = Vector3.Scale(transform.forward, DashDistance * new Vector3((Mathf.Log(1f / (Time.deltaTime * _body.drag + 1)) / -Time.deltaTime), 0, (Mathf.Log(1f / (Time.deltaTime * _body.drag + 1)) / -Time.deltaTime)));
+            _body.AddForce(dashVelocity, ForceMode.VelocityChange);
+        }
     }
 
     void OnCollisionStay()
