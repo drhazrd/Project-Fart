26a775f1fa7d6dc5e1e153539d6ffbf2ee57aa8e
Nick Hatcher Pro <nick@colossalrobot.com>
2020-04-11 19:19:10 -0400

    Promising updates...

---
+++ Assets/Scripts/Controllers-Managers/DoorControllers.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorControllers : MonoBehaviour
+{
+    public int id;
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        OdinEventSystem.current.onDoorTriggerEnter += OnDoorWayOpen;
+        OdinEventSystem.current.onDoorTriggerExit += OnDoorWayClose;
+    }
+
+    void OnDoorWayOpen(int id)
+    {
+        if (id == this.id) { 
+            anim.SetTrigger("OpenClose");
+        }
+    }
+
+    void OnDoorWayClose(int id)
+    {
+        
+        if (id == this.id)
+        {
+            anim.SetTrigger("OpenClose");
+        }
+    }
+    void OnDestroy()
+    {
+        OdinEventSystem.current.onDoorTriggerEnter -= OnDoorWayOpen;
+        OdinEventSystem.current.onDoorTriggerExit -= OnDoorWayClose;
+        
+    }
+}
---
+++ Assets/Scripts/DoorTrigger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorTrigger : MonoBehaviour
+{
+    public int id;
+    private void OnTriggerEnter(Collider other)
+    {
+        OdinEventSystem.current.OpenDoorTrigger(id);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        OdinEventSystem.current.CloseDoorTrigger(id);
+        
+    }
+}
