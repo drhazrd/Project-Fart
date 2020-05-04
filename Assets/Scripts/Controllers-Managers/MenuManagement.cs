0b8b1426b2fdd405d74e731c40640c65be6d2736
Nick Hatcher Pro <nick@colossalrobot.com>
2020-05-03 21:24:30 -0400

    Audio mix

--- Assets/Scripts/Controllers-Managers/MenuManagement.cs
+++ Assets/Scripts/Controllers-Managers/MenuManagement.cs
@@ -8,51 +8,77 @@ using System.Collections.Generic;
 
 public class MenuManagement : MonoBehaviour
 {
+    public static MenuManagement instance;
     Resolution[] resolutions;
     public Dropdown resDropdown;
-    private void Awake()
+    public int levelInt;
+    public int nextLevelInt;
+    string currentMusic;
+    /*private void Awake()
     {
-        FindObjectOfType<AudioManager>().Play("MainMenuTheme");
-    }
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }*/
     void Start()
     {
-        resolutions = Screen.resolutions;
-        resDropdown.ClearOptions();
-        List<string> options = new List<string>();
-
-        int currentResolutionIndex = 0;
-        for (int i = 0; i < resolutions.Length; ++i)
+        if (SceneManager.GetActiveScene().buildIndex == 0)
         {
-            string option = resolutions[i].width + " x " + resolutions[i].height;
-            options.Add(option);
+            levelInt = 0;
+            FindObjectOfType<AudioManager>().Play("MainMenuTheme");
+            currentMusic = "MainMenuTheme";
+            resolutions = Screen.resolutions;
+            resDropdown.ClearOptions();
+            List<string> options = new List<string>();
 
-            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            int currentResolutionIndex = 0;
+            for (int i = 0; i < resolutions.Length; ++i)
             {
-                currentResolutionIndex = i;
+                string option = resolutions[i].width + " x " + resolutions[i].height;
+                options.Add(option);
+
+                if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+                {
+                    currentResolutionIndex = i;
+                }
             }
+            resDropdown.AddOptions(options);
+            resDropdown.value = currentResolutionIndex;
+            resDropdown.RefreshShownValue();
         }
-        resDropdown.AddOptions(options);
-        resDropdown.value = currentResolutionIndex;
-        resDropdown.RefreshShownValue();
-
     }
-
+    private void Update()
+    {
+        levelInt = SceneManager.GetActiveScene().buildIndex;
+        //Change to a save data value
+        nextLevelInt = SceneManager.GetActiveScene().buildIndex + 1;
+    }
     /* Start Menu Code*/
-    public int startScene = 1;
-
     public void PlayGame()
     {
-        SceneManager.LoadScene(startScene);
+        FindObjectOfType<AudioManager>().Stop(currentMusic);
+        SceneManager.LoadScene(nextLevelInt);
+        
     }
 
     public void QuitGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void QuitGameToDesktop()
     {
         Application.Quit();
     }
 
     public void PlayNextLevel()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(nextLevelInt);
     }
 
     /* Settings Menu Code*/
